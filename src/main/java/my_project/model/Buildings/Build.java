c9d6711b8afeb9087dfe46d8881ce5bf1eaf7844
MrAnanassaft <alex20jiang07@gmail.com>
2024-06-02 21:41:21 +0200

    Changes by Samuel

--- src/main/java/my_project/model/Buildings/Build.java
+++ src/main/java/my_project/model/Buildings/Build.java
@@ -4,6 +4,8 @@ import KAGO_framework.model.GraphicalObject;
 import my_project.model.Player;
 
 public abstract class Build extends GraphicalObject {
+    public int buildhealth;
+
     public Build(double x, double y) {
         this.x = x;
         this.y = y;
